031425da9418b28bddbc58fbd9d6408bc819158d
Jesper Petersen <info@ordklar.dk>
2019-08-23 19:20:29 +0200

    Add shoot + car

---
+++ Assets/Script/Hit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hit : MonoBehaviour
+{
+
+    //public void OnTriggerExit(Collider other)
+    //{
+    //    print("exit");
+    //}
+
+    public void OnTriggerEnter(Collider item)
+    {
+        if(item.gameObject.tag == "Player")
+        {
+            print("Picked up");
+            //Destroy(gameObject);
+        }
+        else
+        {
+            print("error");
+        }
+    }
+}
